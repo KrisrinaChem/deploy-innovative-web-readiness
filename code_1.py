636c63174825693d0483c792f531b8f318b6054e
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-12-06 00:37:24 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,25 @@
-main();
-
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-    return data;
-}
-
-}
-    const data = [];
-    const data = generateRandomData();
-    data.forEach(item => {
     }
-        console.log("Random Number: " + item);
-function generateRandomData() {
-function main() {
-    });
+            System.out.println("Random Number: " + item);
+        }
+        for (int i = 0; i < 10; i++) {
+
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        int[] data = new int[10];
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+}
+        for (int item : data) {        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+        for (int item : data) {
+}
+    public static void main(String[] args) {
+        }
+    }
+        for (int i = 0; i < 10; i++) {
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
