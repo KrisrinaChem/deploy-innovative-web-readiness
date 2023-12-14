c4e93442310a6cd408fd092300fea2f61c8934ab
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-12-14 09:39:34 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,27 @@
+def generate_random_data():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+
+    main()
+import random
+
+if __name__ == "__main__":
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    return data
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
+
+def main():
+
+    return data
+def generate_random_data():
+    main()
+
+import random
+
+    for item in data:
