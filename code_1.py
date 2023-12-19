8d3fa3164b9aef34588fbf5173961eb067659b1c
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-12-19 23:36:36 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,13 @@
-    data = generate_random_data()
 if __name__ == "__main__":
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
 import random
 
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
         print(f"Random Number: {item}")
+def generate_random_data():
 
     for item in data:
-    return data
-
 
 def main():
+    main()
+    data = generate_random_data()
